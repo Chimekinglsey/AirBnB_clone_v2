3f20234e0e0bf5ef192ea715b47c10c9a6128a6e
EnGentech <engen.inyang@gmail.com>
2023-06-21 15:23:38 -0700

    modified

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,10 +8,8 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-<<<<<<< HEAD
         if cls is None:
             return FileStorage.__objects
         else:
@@ -21,16 +19,11 @@ class FileStorage:
                     print(value.__class__.__name__)
                     new_dict[key] = value
             return new_dict
-=======
-        if cls is None or cls not in FileStorage.__objects:
-            return self.__objects
-        else:
-            return FileStorage.__objects[cls]
->>>>>>> 39638a19962012c30a233bdffaf158a2d8b2ebea
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
@@ -39,6 +32,7 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -69,7 +63,7 @@ class FileStorage:
             pass
         else:
             for key, value in self.__objects.items():
-                del(self.__objects[key])
+                del (self.__objects[key])
                 self.__objects = self.__objects
                 break
             return self.__objects
