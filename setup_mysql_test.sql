7ce8ac912a8fb300ea1d5242920540d3514ed248
chimekinglsey <onlinekingsley@gmail.com>
2023-06-21 13:52:17 +0000

    setup script for hbnb_test_db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,6 +2,6 @@
 -- we can call db_names directly of enclosed in `` but not ''
 -- the semi-colon at the end of a line is not mandatory but you know manual-man
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test@localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test@localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test@localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
