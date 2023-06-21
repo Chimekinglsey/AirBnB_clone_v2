89bf92855229fa8fdb00c561ed66af717373e2a2
chimekinglsey <onlinekingsley@gmail.com>
2023-06-21 15:00:02 +0000

    update On FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,9 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None or cls not in FileStorage.__objects:
+        if cls is None:
+            return FileStorage.__objects
+        elif cls not in FileStorage.__objects:
             pass
         else:
             return FileStorage.__objects[cls]
