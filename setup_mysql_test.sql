74edc61d1f46966d7753d567fd6b177d856ae433
chimekinglsey <onlinekingsley@gmail.com>
2023-06-21 13:41:14 +0000

    setup script for hbnb_test_db

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- A script that prepares a MySQL server for the project
+-- we can call db_names directly of enclosed in `` but not ''
+-- the semi-colon at the end of a line is not mandatory but you know manual-man
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test@localhost' IDENTIFIES BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO `hbnb_test`;
+GRANT SELECT ON `performance_schema`.* TO `hbnb_test`;
