a18abb58ccd59d15559ae7959473257b6efe2fe4
Chime Kingsley <114360393+Chimekinglsey@users.noreply.github.com>
2023-07-10 02:47:17 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
-from datetime import datetime
+"""this scrip is written with shutil to compress files"""
+
 from fabric.api import local
+from datetime import datetime
+
+current_time = datetime.now()
+new_time = current_time.strftime('%Y%m%d%H%M%S')
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    """define a function to pack all the files into an archive"""
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz \
+        web_static".format(new_time))
+
+        return ("versions/web_static_{}".format(new_time))
+    except Exception:
         return None
-    return file
