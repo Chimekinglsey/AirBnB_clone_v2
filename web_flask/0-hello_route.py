5cb741257d35e486acc9cb4eb6e1c2e760195b55
chimekinglsey <onlinekingsley@gmail.com>
2023-07-23 19:14:08 +0000

    New Commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,19 @@
 """
 0. Hello Flask!
 This module creates a simple flask application that listens at 0.0.0.0 port 5000
+for all connections
 """
 
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def say_hello():
 	"""This method prints greeting to School"""
 	return "Hello HBNB!"
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
